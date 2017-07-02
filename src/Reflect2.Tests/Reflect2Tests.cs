042a966bbb03c5c28cdb2b521bbfd583e7e091d1
ilias shaikh <iliasashaikh@gmail.com>
2017-07-03 00:30:52 +0100

    Added Instance invocation

---
+++ src/Reflect2.Tests/Reflect2Tests.cs
@@ -0,0 +1,138 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using Trycatchthat;
+using System.Linq.Expressions;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+namespace Trycatchthat.Tests
+{
+
+    public class Arithematic
+    {
+        // instance methods
+        public int Add(int a, int b) => a + b;
+        public int Subract(int a, int b) => a * b;
+        
+        //static methods
+        public static int Multiply(int a, int b) => a * b;
+        public static int Multiply(int a, int b, int c) => a * b * c;
+
+        //actions
+        public int Result { get; set; }
+
+        public void AddToResult(int a) => Result += a;
+    }
+
+    [TestFixture]
+    public class Reflect2Tests
+    {
+
+        public static void Main()
+        {
+            BenchmarkRunner.Run<Reflect2Tests>();
+        }
+
+        [Test]
+        public void TestExpression()
+        {
+            //(a,b)=>ari.Add(a,b)
+            var mi = typeof(Arithematic).GetMethod("Add");
+            var paramExp = new ParameterExpression[2] { Expression.Parameter(typeof(int)), Expression.Parameter(typeof(int)) };
+            var body = MethodCallExpression.Call(Expression.Constant(new Arithematic()), mi, paramExp);
+            var lambda = Expression.Lambda<Func<int,int,int>>(
+                    body,paramExp
+                );
+            var f = lambda.Compile();
+            Assert.That(f(1, 3), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void TestExpression2()
+        {
+            //(a,b)=>ari.Add((int)a,(int)b)
+            var mi = typeof(Arithematic).GetMethod("Add");
+            var paramExp1 = new ParameterExpression[2] { Expression.Parameter(typeof(object)), Expression.Parameter(typeof(object)) };
+            //var paramExp2 = new Expression[2] { Expression.Convert(Expression.Parameter(typeof(object)),typeof(int)),
+            //                                    Expression.Convert(Expression.Parameter(typeof(object)),typeof(int))
+            //                                  };
+
+            var x = Expression.Convert(paramExp1[0], typeof(int));
+            var y = Expression.Convert(paramExp1[1], typeof(int));
+
+            var body = MethodCallExpression.Call(Expression.Constant(new Arithematic()), mi, x, y);
+            var lambda = Expression.Lambda<Func<object, object, int>>(body, paramExp1);
+            var f = lambda.Compile();
+            Assert.That(f(1, 3), Is.EqualTo(4));
+        }
+
+
+        [Test]
+        [Benchmark]
+        public void TestInstanceMethod()
+        {
+            var sut = new Arithematic();
+            var r = Reflect2.Run<Arithematic,int>("Add", sut, 1, 2);
+            //var r2 = Reflect2.Run<Arithematic,int>("Add", sut, 1, 2);
+        }
+
+        [Test]
+        [Benchmark]
+        public void TestInstanceMethodReflection()
+        {
+            var sut = new Arithematic();
+            typeof(Arithematic).GetMethod("Add").Invoke(sut, new object[] { 1, 2 });
+            //typeof(Arithematic).GetMethod("Add").Invoke(sut, new object[] { 1, 2 });
+        }
+
+        [Test]
+        public void TestStaticMethod()
+        {
+
+        }
+
+        [Test]
+        public void TestInstanceAction()
+        {
+
+        }
+
+        [Test]
+        public void TestStaticAction()
+        {
+
+        }
+
+        [Test]
+        public void TestInstancePropertySet()
+        {
+
+        }
+
+        [Test]
+        public void TestInstancePropertyGet()
+        {
+
+        }
+
+        [Test]
+        public void TestStaticPropertySet()
+        {
+
+        }
+
+        [Test]
+        public void TestStaticPropertyGet()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ src/Reflect2/Reflect2.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trycatchthat
+{
+    public static class Reflect2
+    {
+        class MethodLookupInfo
+        {
+            public Type Type { get; set; }
+            public string Name { get; set; }
+            public List<Type> ArgTypes { get; set; }
+
+            public class Comparer : IEqualityComparer<MethodLookupInfo>
+            {
+                public bool Equals(MethodLookupInfo x, MethodLookupInfo y)
+                {
+                    return x.Type == y.Type &&
+                            x.Name == y.Name &&
+                            ((x.ArgTypes == null && y.ArgTypes == null) || Enumerable.SequenceEqual<Type>(x.ArgTypes, y.ArgTypes));
+                }
+
+                public int GetHashCode(MethodLookupInfo lookupInfo)
+                {
+                    int argCount = lookupInfo.ArgTypes == null ? 0 : lookupInfo.ArgTypes.Count;
+                    return 17 * lookupInfo.Type.GetHashCode() * lookupInfo.Name.GetHashCode() * (lookupInfo.ArgTypes.Count+1);
+                }
+            }
+
+            public static MethodLookupInfo Make(string methodName, Type type, params object[] args)
+            {
+                return new MethodLookupInfo { Name = methodName, Type = type, ArgTypes = args.Select(a=>a.GetType()).ToList()};
+            }
+
+        }
+
+        static Dictionary<MethodLookupInfo, Delegate> lookup = new Dictionary<MethodLookupInfo, Delegate>(new MethodLookupInfo.Comparer());
+        public static TReturn Run<TSource, TReturn>(string methodName, TSource instance = default(TSource), params object[] args)
+        {
+            var sourceType = typeof(TSource);
+            var returnType = typeof(TReturn);
+
+            var key = MethodLookupInfo.Make(methodName, sourceType, args);
+            Delegate caller;
+
+            if (lookup.TryGetValue(key, out caller))
+                return Run<TReturn>(caller, args);
+
+            // create an expression (arg0,arg1,arg2...)=>instance.Method((int)arg0,(int)arg1,(int)arg2..);
+            var paramExpressionsDecl = args.Select(a => Expression.Parameter(typeof(object))).ToArray();
+            Expression[] paramExpressionsBody = new Expression[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                paramExpressionsBody[i] = Expression.Convert(paramExpressionsDecl[i], args[i].GetType());
+            }
+
+            var methodInfo = typeof(TSource)
+                                    .GetMethod(methodName,
+                                                System.Reflection.BindingFlags.Instance
+                                                | System.Reflection.BindingFlags.Public
+                                                | System.Reflection.BindingFlags.NonPublic,
+                                                null,
+                                                args == null ? null : args.Select(a => a.GetType()).ToArray(),
+                                                null);                                                
+
+            var bodyExpr = Expression.Call(Expression.Constant(instance), methodInfo, paramExpressionsBody);
+            var lambdaExpr = Expression.Lambda(bodyExpr, paramExpressionsDecl);
+            caller = lambdaExpr.Compile();
+            lookup[key] = caller;
+
+            return Run<TReturn>(caller, args);
+            
+        }
+
+        private static TReturn Run<TReturn>(Delegate caller, params object[] args)
+        {
+            var f2 = caller as Func<object, object, TReturn>;
+            if (f2 != null)
+              return f2(args[0], args[1]);
+
+            throw new NotImplementedException();
+        }
+        private static TReturn Run<TReturn, TArg1, TArg2>(Delegate caller, TArg1 arg1, TArg2 arg2)
+        {
+            var f = caller as Func<TArg1, TArg2, TReturn>;
+            if (f != null)
+                return f(arg1, arg2);
+
+            //switch (args.Length)
+            //{
+            //    case 0:
+            //        var f = caller as Func<TReturn>;
+            //        if (f != null)
+            //            return f();
+            //        break;
+
+            //    case 1:
+            //        var f1 = caller as Func<object, TReturn>;
+            //        if (f1 != null)
+            //            return f1(args[0]);
+            //        break;
+
+            //    case 2:
+            //        var f2 = caller as Func<object, object, TReturn>;
+            //        if (f2 != null)
+            //            return f2(args[0], args[1]);
+            //        break;
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //    case 6:
+            //    default:
+            //        break;
+            //}
+
+            throw new NotImplementedException();
+        }
+
+        public static void Action<TSource>(string methodName, TSource instance = default(TSource), params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TProperty GetProperty<Tsource, TProperty>(string propertyName, Tsource instance= default(Tsource))
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SetProperty<Tsource, TProperty>(string propertyName, Tsource instance = default(Tsource), TProperty value = default(TProperty))
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TSource Create<TSource>(params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
